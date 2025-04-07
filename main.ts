619177be43940d97f6b145be36b864171e0802d9
amitguleria2179 <amitguleriamlzs2179@gmail.com>
2025-04-07 13:12:15 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    # . . . #
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    # # . # #
+    # . # . #
+    # . . . #
+    # . . . #
+    `)
+basic.forever(function () {
+	
+})
